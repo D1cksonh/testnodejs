dfa1b9fe27bc536117fc0bb21e2b15df855acacc
D1cksonh <90432572+D1cksonh@users.noreply.github.com>
2022-03-23 18:48:48 -0400

    f

--- script.js
+++
@@ -1,7 +0,0 @@
-function message(){
-   var outputObj = document.getElementById("output");
-   var msg = prompt("Please enter a message: ", "");
-   outputObj.innerHTML = outputObj.innerHTML + msg + "<hr>";
-}
-
-//  this file isn't being used at the moment
